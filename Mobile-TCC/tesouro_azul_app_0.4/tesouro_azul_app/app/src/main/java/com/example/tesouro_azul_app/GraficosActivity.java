f819753f7385d1fad9d3d86a6d257ca388554306
Carlos <maribslll@gmail.com>
2025-04-10 20:17:29 -0300

    Selecionar graficos

--- Mobile-TCC/tesouro_azul_app_0.4/tesouro_azul_app/app/src/main/java/com/example/tesouro_azul_app/GraficosActivity.java
+++ Mobile-TCC/tesouro_azul_app_0.4/tesouro_azul_app/app/src/main/java/com/example/tesouro_azul_app/GraficosActivity.java
@@ -16,6 +16,12 @@ import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
@@ -58,8 +64,8 @@ public class GraficosActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Ação ao clicar no card de Pizza
                 Toast.makeText(getApplicationContext(), "Gráfico de Pizza selecionado", Toast.LENGTH_SHORT).show();
-                typeGraph = "barra";
-                exibirGraficoSelecionado(typeGraph, barChart, lineChart, pieChart);
+                typeGraph = "pizza";
+                exibirGraficoSelecionado(typeGraph,barChart,lineChart,pieChart);
             }
         });
 
@@ -68,9 +74,8 @@ public class GraficosActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Ação ao clicar no card de Barra
                 Toast.makeText(getApplicationContext(), "Gráfico de Barras selecionado", Toast.LENGTH_SHORT).show();
-                typeGraph = "linha";
-                exibirGraficoSelecionado(typeGraph, barChart, lineChart, pieChart);
-
+                typeGraph = "barra";
+                exibirGraficoSelecionado(typeGraph,barChart,lineChart,pieChart);
             }
         });
 
@@ -79,8 +84,8 @@ public class GraficosActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Ação ao clicar no card de Linha
                 Toast.makeText(getApplicationContext(), "Gráfico de Linhas selecionado", Toast.LENGTH_SHORT).show();
-                typeGraph = "pizza";
-                exibirGraficoSelecionado(typeGraph, barChart, lineChart, pieChart);
+                typeGraph = "linha";
+                exibirGraficoSelecionado(typeGraph,barChart,lineChart,pieChart);
             }
         });
 
@@ -89,6 +94,7 @@ public class GraficosActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // Ação ao clicar no card de Lucro
                 Toast.makeText(getApplicationContext(), "Lucro selecionado", Toast.LENGTH_SHORT).show();
+
             }
         });
 
@@ -108,38 +114,98 @@ public class GraficosActivity extends AppCompatActivity {
             }
         });
 
+    }
 
+    private void exibirBarra(BarChart barChart)
+    {
+        // Oculta os rótulos do eixo da direita
         barChart.getAxisRight().setDrawLabels(false);
 
-
+        // Entradas de exemplo
         ArrayList<BarEntry> entries = new ArrayList<>();
         entries.add(new BarEntry(0, 45f));
         entries.add(new BarEntry(1, 80f));
         entries.add(new BarEntry(2, 65f));
         entries.add(new BarEntry(3, 38f));
 
+        // Configuração do eixo Y (esquerdo)
         YAxis yAxis = barChart.getAxisLeft();
-        yAxis.setAxisMaximum(0f);
+        yAxis.setAxisMinimum(0f); // Corrigido: é Minimum, não Maximum aqui
         yAxis.setAxisMaximum(100f);
         yAxis.setAxisLineWidth(2f);
-        yAxis.setAxisLineColor(android.R.color.black);
+        yAxis.setAxisLineColor(Color.BLACK); // Corrigido: usa Color, não resource ID
         yAxis.setLabelCount(10);
 
+        // Dataset
         BarDataSet dataSet = new BarDataSet(entries, "Subjects");
         dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
 
+        // Dados e atribuição ao gráfico
         BarData barData = new BarData(dataSet);
         barChart.setData(barData);
 
+        // Desabilita a descrição padrão
         barChart.getDescription().setEnabled(false);
-        barChart.invalidate();
 
+        // Eixo X
         barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter());
         barChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
         barChart.getXAxis().setGranularity(1f);
         barChart.getXAxis().setGranularityEnabled(true);
 
+        // Atualiza o gráfico
+        barChart.invalidate();
+    }
 
+    private void exibirPizza(PieChart pieChart)
+    {
+
+        ArrayList<PieEntry> entries = new ArrayList<>();
+        entries.add(new PieEntry(45f, "Miguel"));
+        entries.add(new PieEntry(80f, "Carlos"));
+        entries.add(new PieEntry(65f, "Victor"));
+        entries.add(new PieEntry(38f, "Bea"));
+
+        PieDataSet dataSet = new PieDataSet(entries, "Subjects");
+        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        dataSet.setSliceSpace(3f);
+        dataSet.setValueTextSize(12f);
+
+        PieData pieData = new PieData(dataSet);
+        pieChart.setData(pieData);
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setEntryLabelColor(Color.BLACK);
+        pieChart.setHoleRadius(30f);
+        pieChart.setTransparentCircleRadius(35f);
+        pieChart.invalidate(); // Atualiza o gráfico
+    }
+
+    private void exibirLinha(LineChart lineChart)
+    {
+        ArrayList<Entry> entries = new ArrayList<>();
+        entries.add(new Entry(0, 45f));
+        entries.add(new Entry(1, 80f));
+        entries.add(new Entry(2, 65f));
+        entries.add(new Entry(3, 38f));
+
+        LineDataSet dataSet = new LineDataSet(entries, "Subjects");
+        dataSet.setColor(ColorTemplate.getHoloBlue());
+        dataSet.setCircleColor(ColorTemplate.MATERIAL_COLORS[0]);
+        dataSet.setLineWidth(2f);
+        dataSet.setCircleRadius(5f);
+        dataSet.setValueTextSize(10f);
+
+        LineData lineData = new LineData(dataSet);
+        lineChart.setData(lineData);
+        lineChart.getDescription().setEnabled(false);
+
+        lineChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(Arrays.asList("Miguel", "Carlos", "Victor", "Bea")));
+        lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
+        lineChart.getXAxis().setGranularity(1f);
+        lineChart.getXAxis().setGranularityEnabled(true);
+
+        lineChart.invalidate(); // Atualiza o gráfico
     }
 
     private void exibirGraficoSelecionado(String typeGraph, BarChart barChart, LineChart lineChart, PieChart pieChart)
@@ -153,16 +219,19 @@ public class GraficosActivity extends AppCompatActivity {
             case "barra":
                 barChart.setVisibility(View.VISIBLE);
                // configurarBarChart(barChart);
+                exibirBarra(barChart);
                 break;
 
             case "linha":
                 lineChart.setVisibility(View.VISIBLE);
                // configurarLineChart(lineChart);
+                exibirLinha(lineChart);
                 break;
 
             case "pizza":
                 pieChart.setVisibility(View.VISIBLE);
                // configurarPieChart(pieChart);
+                exibirPizza(pieChart);
                 break;
 
             default:
