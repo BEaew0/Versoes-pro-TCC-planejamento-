760c126e036758e22e15ba7f5ec98059b54b2ea0
Carlos <maribslll@gmail.com>
2025-04-10 19:18:06 -0300

    Selecionar graficos

--- Mobile-TCC/tesouro_azul_app_0.4/tesouro_azul_app/app/src/main/java/com/example/tesouro_azul_app/GraficosActivity.java
+++ Mobile-TCC/tesouro_azul_app_0.4/tesouro_azul_app/app/src/main/java/com/example/tesouro_azul_app/GraficosActivity.java
@@ -1,10 +1,15 @@
 package com.example.tesouro_azul_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
@@ -20,9 +25,11 @@ import java.util.List;
 
 public class GraficosActivity extends AppCompatActivity {
 
+    private String typeGraph;
     //Apenas testando, irei modificar futuramente
     private List<String> xValues = Arrays.asList("Miguel", "Carlos", "Victor", "Bea");
 
+    CardView PizzaCard, BarraCard, LinhaCard, LucroCard,VendasCard,FaturamentoCard;
 
     //Maior parte deste conteudo é teste
     @Override
@@ -30,15 +37,86 @@ public class GraficosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graficos);
 
-        BarChart barChart = findViewById(R.id.chart);
+        BarChart barChart = findViewById(R.id.barChart);
+        LineChart lineChart = findViewById(R.id.lineChart);
+        PieChart pieChart = findViewById(R.id.pieChart);
+
+        CardView PizzaCard = findViewById(R.id.PizzaCard);
+        CardView BarraCard = findViewById(R.id.BarraCard);
+        CardView LinhaCard = findViewById(R.id.LinhaCard);
+        CardView LucroCard = findViewById(R.id.LucroCard);
+        CardView VendasCard = findViewById(R.id.VendasCard);
+        CardView FaturamentoCard = findViewById(R.id.FaturamentoCard);
+
+        // Oculta todos
+        barChart.setVisibility(View.GONE);
+        lineChart.setVisibility(View.GONE);
+        pieChart.setVisibility(View.GONE);
+
+        PizzaCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ação ao clicar no card de Pizza
+                Toast.makeText(getApplicationContext(), "Gráfico de Pizza selecionado", Toast.LENGTH_SHORT).show();
+                typeGraph = "barra";
+                exibirGraficoSelecionado(typeGraph, barChart, lineChart, pieChart);
+            }
+        });
+
+        BarraCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ação ao clicar no card de Barra
+                Toast.makeText(getApplicationContext(), "Gráfico de Barras selecionado", Toast.LENGTH_SHORT).show();
+                typeGraph = "linha";
+                exibirGraficoSelecionado(typeGraph, barChart, lineChart, pieChart);
+
+            }
+        });
+
+        LinhaCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ação ao clicar no card de Linha
+                Toast.makeText(getApplicationContext(), "Gráfico de Linhas selecionado", Toast.LENGTH_SHORT).show();
+                typeGraph = "pizza";
+                exibirGraficoSelecionado(typeGraph, barChart, lineChart, pieChart);
+            }
+        });
+
+        LucroCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ação ao clicar no card de Lucro
+                Toast.makeText(getApplicationContext(), "Lucro selecionado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        VendasCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ação ao clicar no card de Vendas
+                Toast.makeText(getApplicationContext(), "Vendas selecionadas", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        FaturamentoCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ação ao clicar no card de Faturamento
+                Toast.makeText(getApplicationContext(), "Faturamento selecionado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
         barChart.getAxisRight().setDrawLabels(false);
 
 
-        ArrayList <BarEntry> entries = new ArrayList<>();
-        entries.add(new BarEntry(0,45f));
-        entries.add(new BarEntry(1,80f));
-        entries.add(new BarEntry(2,65f));
-        entries.add(new BarEntry(3,38f));
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(0, 45f));
+        entries.add(new BarEntry(1, 80f));
+        entries.add(new BarEntry(2, 65f));
+        entries.add(new BarEntry(3, 38f));
 
         YAxis yAxis = barChart.getAxisLeft();
         yAxis.setAxisMaximum(0f);
@@ -60,5 +138,37 @@ public class GraficosActivity extends AppCompatActivity {
         barChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
         barChart.getXAxis().setGranularity(1f);
         barChart.getXAxis().setGranularityEnabled(true);
+
+
+    }
+
+    private void exibirGraficoSelecionado(String typeGraph, BarChart barChart, LineChart lineChart, PieChart pieChart)
+    {
+        // Oculta todos
+        barChart.setVisibility(View.GONE);
+        lineChart.setVisibility(View.GONE);
+        pieChart.setVisibility(View.GONE);
+
+        switch (typeGraph.toLowerCase()) {
+            case "barra":
+                barChart.setVisibility(View.VISIBLE);
+               // configurarBarChart(barChart);
+                break;
+
+            case "linha":
+                lineChart.setVisibility(View.VISIBLE);
+               // configurarLineChart(lineChart);
+                break;
+
+            case "pizza":
+                pieChart.setVisibility(View.VISIBLE);
+               // configurarPieChart(pieChart);
+                break;
+
+            default:
+                Toast.makeText(getApplicationContext(), "Tipo de gráfico inválido", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
+
