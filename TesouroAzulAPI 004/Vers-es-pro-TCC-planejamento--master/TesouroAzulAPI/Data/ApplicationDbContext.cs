aa68a890101b0268079c146dcfc718bb5c2423b0
Mamac0Albin0 <fonsecasouza73@gmail.com>
2025-05-21 17:33:11 -0300

    Model de "adicionar produto" finalizado e revisado: Tabelas representadas do banco : "TB_PEDIDO_COMPRA" , "TB_ITEM_COMPRA"

--- TesouroAzulAPI 004/Vers-es-pro-TCC-planejamento--master/TesouroAzulAPI/Data/ApplicationDbContext.cs
+++ TesouroAzulAPI 004/Vers-es-pro-TCC-planejamento--master/TesouroAzulAPI/Data/ApplicationDbContext.cs
@@ -14,11 +14,24 @@ namespace TesouroAzulAPI.Data
 
         public DbSet<Fornecedor> Fornecedores { get; set; }
 
+        public DbSet<PedidosCompra> PedidoCompra { get; set; }
+
+        public DbSet<ItensCompra> ItensCompra { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
             // Aqui pode realizar configurações adicionais, como definir chaves primárias compostas, etc.
             // Somente em casos de criar o DB atravez da propia API
+
+            // PedidoCompra 1:N ItemCompra
+            /*      
+             *      CORRIGIR AQUI!!!!
+            modelBuilder.Entity<PedidoCompra>()
+                .HasMany(p => p.ItensCompra)
+                .WithOne(i => i.PedidoCompra)
+                .HasForeignKey(i => i.ID_PEDIDO_FK);
+            */
         }
     }
 }
--- TesouroAzulAPI 004/Vers-es-pro-TCC-planejamento--master/TesouroAzulAPI/Models/PedidoCompra.cs
+++ TesouroAzulAPI 004/Vers-es-pro-TCC-planejamento--master/TesouroAzulAPI/Models/PedidoCompra.cs
@@ -1,15 +1,67 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Contracts;
+
 
 namespace TesouroAzulAPI.Models
 {
     [Table("TB_PEDIDO_COMPRA")]
-    public class PedidoCompra
+    public class PedidosCompra
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID_PEDIDO { get; set; }
+        
+        [Required]
+        public int ID_USUARIO_FK { get; set; }
+
+        [Required]
+        public int ID_FORNECEDOR_FK { get; set; }
+
+        [Required]
+        public DateTime DATA_PEDIDO { get; set;}
+
+        [Required, Column(TypeName ="decimal(5,2)")] // Verificar aqui como fica tamanho para double
+        public decimal QTD_PEDIDO { get; set;}
+
+        [Required, StringLength(50)]
+        public string NOME_PRODUTO_PEDIDO { get; set; }
+
+        [Required, Column(TypeName = "decimal(7,2)")]
+        public decimal VALOR_PEDIDO { get; set;}
+
+        [ForeignKey(nameof(ID_USUARIO_FK))]
+        public Usuario Usuario { get; set; }
+
+        [ForeignKey(nameof(ID_FORNECEDOR_FK))]
+        public Fornecedor Fornecedor { get; set; }
     }
 
     [Table("TB_ITEM_COMPRA")]
-    public class ItemsComrpa
+    public class ItensCompra
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID_ITEM_COMPRA { get; set; }
+
+        [Required]
+        public int ID_PRODUTO_FK { get; set; }
+
+        [Required]
+        public int ID_PEDIDO_FK { get; set; }
+
+        [MaxLength(25)]
+        public string LOTE_COMRPA { get; set; }
+
+        [Required, Column(TypeName ="decimal(8,2)")]
+        public decimal QUANTIDADE_ITEM_COMPRA { get; set;}
+
+        [Required]
+        public int N_ITEM_COMPRA { get; set;}
+
+        [ForeignKey(nameof(ID_PRODUTO_FK))]
+        public Produto Produto { get; set; }
+
+        [ForeignKey(nameof(ID_PEDIDO_FK))]
+        public PedidosCompra PedidoCompra { get; set; }
 
     }
 }
