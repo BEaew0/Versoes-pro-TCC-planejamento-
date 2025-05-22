d4489ac389c6ddf60910d224078b0d0b0da05fc4
Mamac0Albin0 <fonsecasouza73@gmail.com>
2025-05-22 02:10:54 -0300

    Model de "EstoqueProduto.cs" montado. estrutura do controller "PedidoCompraController.cs" parcial montada

---
+++ TesouroAzulAPI 004/Vers-es-pro-TCC-planejamento--master/TesouroAzulAPI/Controllers/PedidoCompraController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TesouroAzulAPI.Data;
+
+namespace TesouroAzulAPI.Controllers
+{
+    [Route ("api/PeidoCompra"), ApiController]
+    public class PedidoCompraController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        // Instandiando o context da ApplicationDbContext para _context
+        public PedidoCompraController(ApplicationDbContext context) { _context = context; }
+
+        // Para este controller é necessário criar o models de EstoqueProduto, seguir com lógica aqui mais tarde
+    }
+}
