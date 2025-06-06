f1343772c70a236054ad157b5c218684efcd28ad
Mamac0Albin0 <fonsecasouza73@gmail.com>
2025-06-06 17:35:27 -0300

    Lógica de lucro avançada ( não funcional ) e adicionado model de metas, segue especificações : Model de lucro criado; Controller de lucro criado; Data corrigido;

---
+++ TesouroAzulAPI 004/Vers-es-pro-TCC-planejamento--master/TesouroAzulAPI/Controllers/LucroController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TesouroAzulAPI.Data;
+using TesouroAzulAPI.Dtos;
+
+namespace TesouroAzulAPI.Controllers
+{
+    [Route("api/Lucro")]
+    public class LucroController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        // Instanciando o context do banco
+        public LucroController(ApplicationDbContext context) { _context = context; }
+
+        // POSTs
+        // Criar lucro
+        //  Provavel de não utilizar por ser criado automatico no banco
+
+        // Buscar lucro por campo
+
+        // Buscar ItensLucro por campo 
+
+        // GETs
+        // Buscar lucros de todos usuarios
+
+        // Buscar itens lucro de todos usuarios
+
+        // Buscar lucros do usuario {id}
+
+        // Buscar itens lucro do usuario {id}
+
+        // PATCHs
+        // Alterar valor lucro
+
+
+        // DELETEs
+        // Deletar Lucro
+
+        // Deletar Itens lucro
+
+    }
+}
